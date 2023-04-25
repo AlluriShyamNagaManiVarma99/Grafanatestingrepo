396203c0d5c86a9278e6f373ea4af6761fb17b31
AlluriShyamNagaManiVarma99 <92780785+AlluriShyamNagaManiVarma99@users.noreply.github.com>
2023-04-25 09:03:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,6 +8,7 @@ terraform {
 }
 
 provider "grafana" {
+  alias = "cloud"
   url  = "http://34.71.66.57:3000"
   auth = "eyJrIjoiYm5xenBNUmxQcm9rUTZCQmp3WFA1Y1RhZkE0VWJKcjMiLCJuIjoiTWFuaSIsImlkIjoxfQ=="
 }
