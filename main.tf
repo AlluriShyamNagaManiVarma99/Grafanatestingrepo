2e020954a81ecf6f06c93bf734dc08cda50c9257
AlluriShyamNagaManiVarma99 <92780785+AlluriShyamNagaManiVarma99@users.noreply.github.com>
2023-04-25 09:00:45 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+terraform {
+    required_providers {
+        grafana{
+            source = "grafana/grafana"
+            version = "1.24.0"
+        }
+    }
+}
+
+provider "grafana" {
+  url  = "http://34.71.66.57:3000"
+  auth = "eyJrIjoiYm5xenBNUmxQcm9rUTZCQmp3WFA1Y1RhZkE0VWJKcjMiLCJuIjoiTWFuaSIsImlkIjoxfQ=="
+}
