20a16003470936b57ee346157d5f7ba0460ac163
AlluriShyamNagaManiVarma99 <92780785+AlluriShyamNagaManiVarma99@users.noreply.github.com>
2023-05-11 16:50:57 +0530

    Update dashboards.tf

--- dashboards.tf
+++ dashboards.tf
@@ -1,15 +1,1 @@
-resource "grafana_folder" "ElasticSearch" {
-   provider = grafana.cloud
-   title = "Terraformdasboards"
-}
-
-
-
-resource "grafana_dashboard" "elasticsearch" {
-   provider = grafana.cloud
-
-   for_each    = fileset("./../dasboards", "*.json")
-   config_json = file("./../dasboards/${each.key}")
-   folder      = grafana_folder.ElasticSearch.id
-}
 
