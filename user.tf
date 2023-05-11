719242511e8d295c9fca3e15208ff5817c2e888a
AlluriShyamNagaManiVarma99 <92780785+AlluriShyamNagaManiVarma99@users.noreply.github.com>
2023-05-11 16:51:22 +0530

    Update user.tf

--- user.tf
+++ user.tf
@@ -1,8 +1,1 @@
-resource "grafana_user" "staff" {
-  provider = grafana.cloud
-  email    = "developer123@example.com"
-  name     = "Developer"
-  login    = "Developer"
-  password = "1234567890"
-  is_admin = false
-}
+
